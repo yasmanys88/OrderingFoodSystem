9f6ae1d435a76bacc150cade3368be4f02e81de8
yasmanys88 <yasmanys88@gmail.com>
2024-04-25 09:03:12 -0400

    Project Structure

---
+++ src/main/java/com/ordering/OrderingFoodApplication.java
@@ -0,0 +1,13 @@
+package com.ordering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderingFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderingFoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ordering/OrderingFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ordering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderingFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
