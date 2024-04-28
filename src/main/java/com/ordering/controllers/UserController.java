a9c983d5ec2bf23e8e7f57758738ff03e355db63
yasmanys88 <yasmanys88@gmail.com>
2024-04-28 12:35:13 -0400

    Using SLF4J to log log messages based on operations performed

--- src/main/java/com/ordering/controllers/UserController.java
+++ src/main/java/com/ordering/controllers/UserController.java
@@ -4,11 +4,12 @@ import com.ordering.documents.User;
 import com.ordering.dto.UserDto;
 import com.ordering.services.UserService;
 import jakarta.validation.constraints.Email;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+@Slf4j
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
@@ -20,22 +21,27 @@ private UserService userService;
 
     @GetMapping()
     public ResponseEntity<?> getAllUsers() {
+        log.info("Search for all Users");
         return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
     }
     @GetMapping("/{email}")
-    public ResponseEntity<?> getUserById(@PathVariable String email) {
+    public ResponseEntity<?> getUserByEmail(@PathVariable String email) {
+        log.info("User Search by Email: "+ email);
         return new ResponseEntity<>(userService.getUserByEmail(email), HttpStatus.OK);
     }
     @PostMapping("")
     public ResponseEntity<?> createUser(@RequestBody UserDto user) {
+        log.info("Creating user: "+ user.getName());
         return new ResponseEntity<>(userService.createUser(user),HttpStatus.CREATED);
     }
     @PutMapping("")
     public ResponseEntity<?> updateUser(@RequestBody UserDto user) {
+        log.info("Updating user: "+ user.getName());
         return new ResponseEntity<>(userService.updateUser(user),HttpStatus.OK);
     }
     @DeleteMapping("/{email}")
     public ResponseEntity<?> deleteUser(@PathVariable String email) {
+        log.info("Deleting user with email: "+ email);
         return new ResponseEntity<>(userService.deleteUserByEmail(email),HttpStatus.OK);
     }
 
--- src/main/java/com/ordering/services/servicesImpl/UserServiceImpl.java
+++ src/main/java/com/ordering/services/servicesImpl/UserServiceImpl.java
@@ -4,6 +4,7 @@ import com.ordering.documents.User;
 import com.ordering.dto.UserDto;
 import com.ordering.repositories.UserRepo;
 import com.ordering.services.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,7 +12,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 import java.util.List;
 import java.util.stream.Collectors;
-
+@Slf4j
 @Service
 public class UserServiceImpl implements UserService {
 
@@ -24,39 +25,48 @@ public class UserServiceImpl implements UserService {
     @Override
     public List<?> getAllUsers() {
         List<User> users = userRepo.findAll();
+        log.info("Converting users to DTO format");
         return users.stream()
                 .map(this::convertToDTO)
                 .collect(Collectors.toList());
     }
     @Override
     public UserDto createUser(UserDto user) {
+        log.info("Converting UserDto to User format and creating user");
         return this.convertToDTO(userRepo.save(this.convertToDocument(user)));
     }
     @Override
     public String deleteUserByEmail(String email) {
         if (!userRepo.existsByEmail(email)) {
+            log.error("User not found with email: " + email);
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found with email: " + email);
         }
         userRepo.deleteByEmail(email);
+        log.info("User with email: " + email +" was deleted");
         return "User with email: " + email +" was deleted";
     }
     @Override
     public UserDto updateUser(UserDto updatedUser) {
 
         if (!userRepo.existsByEmail(updatedUser.getEmail())) {
+            log.error("User not found with email: " + updatedUser.getEmail());
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found with email: " + updatedUser.getEmail());
         }
         User userDb= userRepo.findByEmail(updatedUser.getEmail());
+        log.info("Updating user fields");
         userDb.setDeliveryAddress(updatedUser.getDeliveryAddress());
         userDb.setEmail(updatedUser.getEmail());
         userDb.setName(updatedUser.getName());
+
         return this.convertToDTO(userRepo.save(userDb));
     }
     @Override
     public UserDto getUserByEmail(String email) {
         if (!userRepo.existsByEmail(email)) {
+            log.error("User not found with email: " + email);
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found with email: " + email);
         }
+        log.info("Looking for user information with email: "+ email);
         return this.convertToDTO(userRepo.findByEmail(email));
     }
     private UserDto convertToDTO(User user) {
