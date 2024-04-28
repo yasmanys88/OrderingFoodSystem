0ce532fb63d7b02cdda12f3a5341ca02bc9211bf
yasmanys88 <yasmanys88@gmail.com>
2024-04-28 12:36:12 -0400

    Using SLF4J to log log messages based on operations performed

--- src/main/java/com/ordering/controllers/UserController.java
+++ src/main/java/com/ordering/controllers/UserController.java
@@ -1,9 +1,7 @@
 package com.ordering.controllers;
 
-import com.ordering.documents.User;
 import com.ordering.dto.UserDto;
 import com.ordering.services.UserService;
-import jakarta.validation.constraints.Email;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
