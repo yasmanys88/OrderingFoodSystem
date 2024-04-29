c12721d3ae7d29b14dae4b759e225782dbe2648c
yasmanys88 <yasmanys88@gmail.com>
2024-04-29 01:39:16 -0400

    creating the validation class ErrorValidationComponent

---
+++ src/main/java/com/ordering/validations/ErrorValidationComponent.java
@@ -0,0 +1,27 @@
+package com.ordering.validations;
+
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+@NoArgsConstructor
+@Slf4j
+public class ErrorValidationComponent {
+
+    public ResponseEntity<?> validationErrors(BindingResult result) {
+        Map<String, String> errorMap = new HashMap<>();
+        for (FieldError error : result.getFieldErrors()) {
+            errorMap.put(error.getField(),error.getDefaultMessage());
+        }
+        log.error(result.getErrorCount()+" errors were found in the validated fields");
+        return new ResponseEntity<>(errorMap, HttpStatus.BAD_REQUEST);
+    }
+}
