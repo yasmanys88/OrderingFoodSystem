b47769ea3fa526e49dd76c5491d154d823917c93
yasmanys88 <yasmanys88@gmail.com>
2024-05-11 00:44:32 -0400

    Updating Exception

--- src/main/java/com/ordering/services/servicesImpl/RestaurantServiceImpl.java
+++ src/main/java/com/ordering/services/servicesImpl/RestaurantServiceImpl.java
@@ -2,6 +2,7 @@ package com.ordering.services.servicesImpl;
 
 import com.ordering.documents.Restaurant;
 import com.ordering.dto.RestaurantDto;
+import com.ordering.exception.NotFoundException;
 import com.ordering.repositories.RestaurantRepo;
 import com.ordering.services.RestaurantService;
 import lombok.extern.slf4j.Slf4j;
@@ -27,48 +28,63 @@ public class RestaurantServiceImpl implements RestaurantService {
 
     @Override
     public List<RestaurantDto> getAllRestaurant() {
-        List<Restaurant> restaurantList = restaurantRepo.findAll();
-        log.info("Converting Restaurant  List to DTO format");
-        return restaurantList.stream().map(this::convertToDTO).collect(Collectors.toList());
+        try {
+            List<Restaurant> restaurantList = restaurantRepo.findAll();
+            log.info("Converting Restaurant  List to DTO format");
+            return restaurantList.stream().map(this::convertToDTO).collect(Collectors.toList());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public RestaurantDto getRestaurantByName(String name) {
-        if (!restaurantRepo.existsByName(name))
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Restaurant not found with name: " + name);
-        return this.convertToDTO(restaurantRepo.findByName(name));
+        if (!restaurantRepo.existsByName(name)) throw new NotFoundException("Restaurant not found with name: " + name);
+        try {
+            return this.convertToDTO(restaurantRepo.findByName(name));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public RestaurantDto createRestaurant(RestaurantDto restaurantDto) {
-        log.info("Converting RestaurantDto to Restaurant format and creating Restaurant");
-        return this.convertToDTO(restaurantRepo.save(this.convertToDocument(restaurantDto)));
+        try {
+            log.info("Converting RestaurantDto to Restaurant format and creating Restaurant");
+            return this.convertToDTO(restaurantRepo.save(this.convertToDocument(restaurantDto)));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public RestaurantDto updateRestaurant(RestaurantDto restaurantDto) {
-        if (!restaurantRepo.existsByName(restaurantDto.getName())) {
-            log.error("Restaurant not found with Name: " + restaurantDto.getName());
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Restaurant not found with Name: " + restaurantDto.getName());
+        if (!restaurantRepo.existsByName(restaurantDto.getName()))
+            throw new NotFoundException("Restaurant not found with Name: " + restaurantDto.getName());
+
+        try {
+            Restaurant restaurant = restaurantRepo.findByName(restaurantDto.getName());
+            log.info("Updating Restaurant fields");
+            restaurant.setName(restaurantDto.getName());
+            restaurant.setAddress(restaurantDto.getAddress());
+            restaurant.setContact_info(restaurantDto.getContact_info());
+            restaurant.setMenu(restaurantDto.getMenu());
+            return this.convertToDTO(restaurantRepo.save(restaurant));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
-        Restaurant restaurant = restaurantRepo.findByName(restaurantDto.getName());
-        log.info("Updating Restaurant fields");
-        restaurant.setName(restaurantDto.getName());
-        restaurant.setAddress(restaurantDto.getAddress());
-        restaurant.setContact_info(restaurantDto.getContact_info());
-        restaurant.setMenu(restaurantDto.getMenu());
-        return this.convertToDTO(restaurantRepo.save(restaurant));
     }
 
     @Override
     public String deleteRestaurantByName(String name) {
-        if (!restaurantRepo.existsByName(name)) {
-            log.error("Restaurant not found with name: " + name);
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Restaurant not found with email: " + name);
+        if (!restaurantRepo.existsByName(name)) throw new NotFoundException("Restaurant not found with Name: " + name);
+        try {
+            restaurantRepo.deleteByName(name);
+            log.info("Restaurant with name: " + name + " was deleted");
+            return "Restaurant with name: " + name + " was deleted";
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
-        restaurantRepo.deleteByName(name);
-        log.info("Restaurant with name: " + name + " was deleted");
-        return "Restaurant with name: " + name + " was deleted";
     }
 
     private RestaurantDto convertToDTO(Restaurant restaurant) {
