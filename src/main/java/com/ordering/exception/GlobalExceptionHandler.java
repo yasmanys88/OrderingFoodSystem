33c3c49db9038702b152de0d390aca1dfac5d118
yasmanys88 <yasmanys88@gmail.com>
2024-05-11 00:09:41 -0400

    Updating Exception

--- src/main/java/com/ordering/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ordering/exception/GlobalExceptionHandler.java
@@ -2,17 +2,12 @@ package com.ordering.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.nio.file.attribute.UserPrincipalNotFoundException;
 import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler  extends ResponseEntityExceptionHandler {
@@ -22,7 +17,7 @@ public class GlobalExceptionHandler  extends ResponseEntityExceptionHandler {
         ExceptionDetails exceptionDetails= new ExceptionDetails(LocalDateTime.now(), exception.getMessage(), webRequest.getDescription(true));
         return new ResponseEntity<>(exceptionDetails,HttpStatus.INTERNAL_SERVER_ERROR);
     }
-    @ExceptionHandler(UserNotFoundException.class)
+    @ExceptionHandler(NotFoundException.class)
     public final  ResponseEntity<?> handleUserNotFoundException(Exception exception, WebRequest webRequest){
         ExceptionDetails exceptionDetails= new ExceptionDetails(LocalDateTime.now(), exception.getMessage(), webRequest.getDescription(true));
         return new ResponseEntity<>(exceptionDetails,HttpStatus.NOT_FOUND);
